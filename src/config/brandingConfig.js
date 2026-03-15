3032db6fafad3584c3af2d707d3110147c9afe47
MWijesekara <menurawijesekara17@gmail.com>
2026-03-15 09:34:28 +0530

    feat: Introduce branding configuration and import a new theme stylesheet.

---
+++ src/config/brandingConfig.js
@@ -0,0 +1,6 @@
+export const BRANDING = {
+    companyName: "BioGenHoldings",
+    tagline: "The Future of Healthcare",
+    logoUrl: "/logo.svg", // Default placeholder
+    footerText: "© BioGenHoldings 2026. All rights reserved."
+};
